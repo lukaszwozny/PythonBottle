dcba540f75df23c8d81af12d0139057fe0d37867
cp24-Komputer\cp24 <kontakt@lukaszwozny.com>
2017-10-18 01:56:25 +0200

    Add pytube

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from bottle import route, run, template
+from bottle import route, run, template, request
+from pytube import YouTube
 
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+@route('/')
+def index():
+    url = request.query['url']
+    try:
+        yt = YouTube(url=url)
+        extension = 'mp4'
+        video = yt.filter(extension)[0]
+        return video.url
+    except ValueError:
+        return "ValueError"
+    except AttributeError:
+        return "AttributeError"
+    except IndexError:
+        return "IndexError"
+    return "Error"
 
 
 run(host='0.0.0.0', port=8082)
