378d94f2d2eb11867d2ffccf5684ce7cc3067798
cp24-Komputer\cp24 <kontakt@lukaszwozny.com>
2017-10-18 01:45:43 +0200

    change port and host

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ from bottle import route, run, template
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
-run(host='localhost', port=8080)
+
+run(host='0.0.0.0', port=8082)
